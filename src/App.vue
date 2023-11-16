fafe6fb8beb04122f23082c190335f40f11ed679
sakura <11854578@qq.com>
2023-11-16 22:58:15 +0800

    feat: 搭建项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SkButton from '@/components/Button/index.vue'
+</script>
+
+<template>
+  <sk-button type="primary" plain>Primary</sk-button>
+</template>
+
+<style scoped></style>
